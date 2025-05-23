9dcbbf19193d3865637092f7f64f85a2f9764a42
onyinyeofili <onyinyeofili209@gmail.com>
2025-05-23 21:41:37 +0100

    noted (note app)

--- src/components/Side.jsx
+++ src/components/Side.jsx
@@ -32,7 +32,7 @@ function Side() {
                 <span>All Notes</span>
               </li>
 
-              <li>
+              <li className="active">
                 <svg xmlns="http://www.w3.org/2000/svg" className="side-body-svg" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h4l3 3h7a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2"/></svg>
                 <span>Daily</span>
               </li>
