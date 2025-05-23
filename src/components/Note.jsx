bed03bcf8ad2472219d6bdc7b8182c5436315a5b
onyinyeofili <onyinyeofili209@gmail.com>
2025-05-23 18:36:17 +0100

    noted (note app)

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -14,7 +14,19 @@ function Note() {
 
                     <div className="note-main">
                         <p className="note-header">All Notes</p>
-                        <p className="note-text">Last edited today at 12:45</p>
+                        <p className="note-text">Friday</p>
+                    </div>
+                </div>
+
+                <div className="note note-black note-overlay note-new">
+                    <div className="note-img">
+                        <img src="/src/assets/images/note-black.png" />  
+                        <div className="note-overlay-text">
+                            <p className="note-overlag-svg">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-3-3v6M3 5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
+                            </p>
+                            <p className="note-overlay-p">8/05/25</p>  
+                        </div>
                     </div>
                 </div>
 
@@ -28,7 +40,7 @@ function Note() {
 
                     <div className="note-main">
                         <p className="note-header">Daily</p>
-                        <p className="note-text">Last edited today at 12:45</p>
+                        <p className="note-text">5/11/25</p>
                     </div>
                 </div>
 
@@ -45,21 +57,9 @@ function Note() {
 
                     <div className="note-main">
                         <p className="note-header">Personal</p>
-                        <p className="note-text">Last edited today at 12:45</p>
+                        <p className="note-text">Wednesday</p>
                     </div>
-                </div>
-
-                <div className="note note-black note-overlay note-new">
-                    <div className="note-img">
-                        <img src="/src/assets/images/note-black.png" />  
-                        <div className="note-overlay-text">
-                            <p className="note-overlag-svg">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-3-3v6M3 5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
-                            </p>
-                            <p className="note-overlay-p">Create Note</p>  
-                        </div>
-                    </div>
-                </div>
+                </div>                
             </div>
         </div>
     );
