5bc6945e9b0a5a4d14566efe6d987ee975ea791a
onyinyeofili <onyinyeofili209@gmail.com>
2025-06-20 23:18:26 +0100

    noted (note app)

--- src/components/Folder/GridControls/GridLayout.jsx
+++ src/components/Folder/GridControls/GridLayout.jsx
@@ -24,6 +24,196 @@ function GridLayout ({ data = [], toggleStar }) {
                                                         </a>
                                                     </div>
                                                 </div>
+
+                                                <div className="folder-layout-bottom folder-link-bottom">                                                
+                                                    <div className="folder-link-bottom-star" onClick={() => toggleStar(folder.id)}>
+                                                        {folder.starred ? (
+                                                            <svg 
+                                                                xmlns="http://www.w3.org/2000/svg" 
+                                                                className="folder-bottom-star-svg" 
+                                                                viewBox="0 0 24 24"
+                                                            >
+                                                                <path 
+                                                                    fill="currentColor" 
+                                                                    d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
+                                                                />
+                                                            </svg>
+                                                        ) : (
+                                                            <svg 
+                                                                xmlns="http://www.w3.org/2000/svg" 
+                                                                className="folder-bottom-star-svg" 
+                                                                viewBox="0 0 24 24"
+                                                            >
+                                                                <path 
+                                                                    fill="none" 
+                                                                    stroke="currentColor" 
+                                                                    strokeLinecap="round" 
+                                                                    strokeLinejoin="round" 
+                                                                    strokeWidth="2" 
+                                                                    d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
+                                                                />
+                                                            </svg>
+                                                        )}
+
+                                                        <p  className="folder-bottom-copy-text">Star</p>
+                                                    </div>
+
+                                                    <div className="folder-link-bottom-copy" onClick={() => toggleStar(folder.id)}>
+                                                        {folder.starred ? (
+                                                            <svg 
+                                                                xmlns="http://www.w3.org/2000/svg" 
+                                                                className="folder-bottom-copy-svg" 
+                                                                viewBox="0 0 24 24"
+                                                            >
+                                                                <g 
+                                                                    fill="none" 
+                                                                    stroke="currentColor" 
+                                                                    strokeLinecap="round" 
+                                                                    strokeLinejoin="round" 
+                                                                    strokeWidth={2}
+                                                                >
+                                                                    <path 
+                                                                        d="M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z"
+                                                                    >
+                                                                    </path>
+                                                                    <path 
+                                                                        d="M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1"
+                                                                    >
+                                                                    </path>
+                                                                </g>
+                                                            </svg>
+                                                        ) : (
+                                                            <svg 
+                                                                xmlns="http://www.w3.org/2000/svg" 
+                                                                className="folder-bottom-copy-svg" 
+                                                                viewBox="0 0 24 24"
+                                                            >
+                                                                <g 
+                                                                    fill="none" 
+                                                                    stroke="currentColor" 
+                                                                    strokeLinecap="round" 
+                                                                    strokeLinejoin="round" 
+                                                                    strokeWidth={2}
+                                                                >
+                                                                    <path 
+                                                                        d="M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z"
+                                                                    >
+                                                                    </path>
+                                                                    <path 
+                                                                        d="M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1"
+                                                                    >
+                                                                    </path>
+                                                                </g>
+                                                            </svg>
+                                                        )}
+
+                                                        <p  className="folder-bottom-copy-text">Copy</p>
+                                                    </div>
+
+                                                    <div className="folder-link-bottom-copy" onClick={() => toggleStar(folder.id)}>
+                                                        {folder.starred ? (
+                                                            <svg 
+                                                                xmlns="http://www.w3.org/2000/svg" 
+                                                                className="folder-bottom-copy-svg" 
+                                                                viewBox="0 0 24 24"
+                                                            >
+                                                                <g 
+                                                                    fill="none" 
+                                                                    stroke="currentColor" 
+                                                                    strokeLinecap="round" 
+                                                                    strokeLinejoin="round" 
+                                                                    strokeWidth={2}
+                                                                >
+                                                                    <path 
+                                                                        d="M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z"
+                                                                    >
+                                                                    </path>
+                                                                    <path 
+                                                                        d="M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1"
+                                                                    >
+                                                                    </path>
+                                                                </g>
+                                                            </svg>
+                                                        ) : (
+                                                            <svg 
+                                                                xmlns="http://www.w3.org/2000/svg" 
+                                                                className="folder-bottom-copy-svg" 
+                                                                viewBox="0 0 24 24"
+                                                            >
+                                                                <g 
+                                                                    fill="none" 
+                                                                    stroke="currentColor" 
+                                                                    strokeLinecap="round" 
+                                                                    strokeLinejoin="round" 
+                                                                    strokeWidth={2}
+                                                                >
+                                                                    <path 
+                                                                        d="M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z"
+                                                                    >
+                                                                    </path>
+                                                                    <path 
+                                                                        d="M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1"
+                                                                    >
+                                                                    </path>
+                                                                </g>
+                                                            </svg>
+                                                        )}
+
+                                                        <p  className="folder-bottom-copy-text">Edit</p>
+                                                    </div>
+
+                                                    <div className="folder-link-bottom-copy" onClick={() => toggleStar(folder.id)}>
+                                                        {folder.starred ? (
+                                                            <svg 
+                                                                xmlns="http://www.w3.org/2000/svg" 
+                                                                className="folder-bottom-copy-svg" 
+                                                                viewBox="0 0 24 24"
+                                                            >
+                                                                <g 
+                                                                    fill="none" 
+                                                                    stroke="currentColor" 
+                                                                    strokeLinecap="round" 
+                                                                    strokeLinejoin="round" 
+                                                                    strokeWidth={2}
+                                                                >
+                                                                    <path 
+                                                                        d="M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z"
+                                                                    >
+                                                                    </path>
+                                                                    <path 
+                                                                        d="M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1"
+                                                                    >
+                                                                    </path>
+                                                                </g>
+                                                            </svg>
+                                                        ) : (
+                                                            <svg 
+                                                                xmlns="http://www.w3.org/2000/svg" 
+                                                                className="folder-bottom-copy-svg" 
+                                                                viewBox="0 0 24 24"
+                                                            >
+                                                                <g 
+                                                                    fill="none" 
+                                                                    stroke="currentColor" 
+                                                                    strokeLinecap="round" 
+                                                                    strokeLinejoin="round" 
+                                                                    strokeWidth={2}
+                                                                >
+                                                                    <path 
+                                                                        d="M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z"
+                                                                    >
+                                                                    </path>
+                                                                    <path 
+                                                                        d="M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1"
+                                                                    >
+                                                                    </path>
+                                                                </g>
+                                                            </svg>
+                                                        )}
+
+                                                        <p  className="folder-bottom-copy-text">Delete</p>
+                                                    </div>
+                                                </div>
                                             </div>
                                         ) : (
                                         <>
