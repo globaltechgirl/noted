4f8ca068d3fb0a45a100a3595c942d2b345c4095
onyinyeofili <onyinyeofili209@gmail.com>
2025-06-29 02:32:12 +0100

    noted (note app)

--- src/components/Folder/GridControls/GridList.jsx
+++ src/components/Folder/GridControls/GridList.jsx
@@ -300,11 +300,15 @@ function GridList ({
                                                         {folder.starred ? (
                                                             <svg 
                                                                 xmlns="http://www.w3.org/2000/svg" 
-                                                                className="folder-right-star-svg" 
+                                                                className="folder-right-starred-svg" 
                                                                 viewBox="0 0 24 24"
                                                             >
                                                                 <path 
-                                                                    fill="currentColor" 
+                                                                    fill="none" 
+                                                                    stroke="currentColor" 
+                                                                    strokeLinecap="round" 
+                                                                    strokeLinejoin="round" 
+                                                                    strokeWidth="2" 
                                                                     d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
                                                                 />
                                                             </svg>
